8be3a7059145f5cfbe3582ca2b4e7bd4d84334b2
k708 <16235627+k708@users.noreply.github.com>
2022-06-11 13:28:54 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import Map from "./map/Map";
+
+export default function App() {
+  return (
+    <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+        integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+        crossorigin=""
+      />
+      <Map />
+    </div>
+  );
+}
